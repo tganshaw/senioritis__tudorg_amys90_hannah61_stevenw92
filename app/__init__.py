eefe828d5c1cf47409c4fcc98bf2609105f6e99b
Tudor Ganshaw <tganshaw@gmail.com>
2026-05-05 15:00:17 +0000

    Starting on figuring out how to do this with JS

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import *
 
 app = Flask(__name__)
 
+app.secret_key = "er34546;'546;'3;'3453453kl345l;45k34905uidkldg593495io;dfop"
+
 @app.route("/")
 def main():
-    return "<title>Senioritis</title>\n<h1>Got This Working</h1><br><br><p>yay</p>"
+    return render_template("jstest.html")
+    # return "<title>Senioritis</title>\n<h1>Got This Working</h1>\n<br>\n<br>\n<p>yay</p>"
     
     
 if __name__ == "__main__":
