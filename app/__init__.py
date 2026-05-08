dafd659bfb112aef3513ef3eb7af2dfc21d08917
Hannah Grimskog-Tran <hannahg61@nycstudents.net>
2026-05-08 14:31:25 +0000

    Add login and registration routes

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,86 @@ def main():
     
     return render_template("jstest.html", testingtesting = data)
     # return "<title>Senioritis</title>\n<h1>Got This Working</h1>\n<br>\n<br>\n<p>yay</p>"
-    
-    
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
+
+@app.route("/login.html")
+def loginhtml():
+    if 'username' in session:
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/register.html")
+def registerhtml():
+    if 'username' in session:
+        return redirect("/")
+    return render_template("register.html")
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+  if 'username' in session:
+      return redirect(url_for('homepage'))
+  if request.method == 'POST':
+    username = request.form.get('username', '').strip()
+    password = request.form.get('password', '')
+
+    if not username or not password:
+      return render_template('login.html', error="Please enter both username and password")
+
+    db = sqlite3.connect(DB_NAME)
+    c = db.cursor()
+    c.execute("SELECT username, password FROM users WHERE username = ?", (username,))
+    user = c.fetchone()
+    db.close()
+
+    if not user or user[1] != password:
+      text = "Login failed. Invalid username or password."
+      return render_template('login.html', error=text)
+
+    session['username'] = username
+    return redirect(url_for('/'))
+
+  return render_template('login.html', error="")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if request.method == "POST":
+    username = request.form.get("username", "").strip()
+    # email = request.form.get("email", "").strip() why would we need an email
+    password = request.form.get("password", "")
+    confirm = request.form.get("confirm", "")
+    reviews=""
+    if not username or not password or not confirm:
+      return render_template("register.html", error="All fields are required!")
+
+    if password != confirm:
+      return render_template("register.html", error="Passwords do not match!")
+
+    db = sqlite3.connect(DB_NAME)
+    c = db.cursor()
+
+    c.execute("SELECT * FROM users WHERE username = ?", (username,))
+    if c.fetchone():
+      db.close()
+      return render_template("register.html", error="Username already taken!")
+
+    c.execute("INSERT INTO users VALUES (?, ?, ?, NULL, NULL, NULL)",
+    (username, password, reviews))
+
+    db.commit()
+    db.close()
+
+    session['username'] = username
+    if 'rated_games' not in session:
+        session['rated_games']=[]
+    session.permanent=True
+    return redirect(url_for("/"))
+
 if __name__ == "__main__":
+    app.debug=True
     app.run()
